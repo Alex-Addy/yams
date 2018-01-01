f889bae6900c017ce08640e41b32aa5299bb838e
Alex Addy <alex.addy@gmail.com>
2018-01-01 16:00:20 -0500

    The rendered pages are now valid html
    Wrapped commonmark output in a minimal html wrapper.
    Set content type so the browser treats it as html.

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,35 @@ use std::io;
 use std::io::Read;
 use std::path::{Path, PathBuf};
 
+use rocket::response::content::Content;
+use rocket::http::ContentType;
+
 #[get("/<path..>")]
-fn pages(path: PathBuf) -> io::Result<String> {
+fn pages(path: PathBuf) -> io::Result<Content<String>> {
+    let full_path = Path::new(SITE_ROOT).join(path.with_extension("md"));
+
     if let Some(ext) = path.extension() {
         if ext == "html" {
-            return get_md_as_html(&Path::new(SITE_ROOT).join(path.with_extension("md")))
+            let rendered = get_md_as_html(&full_path)?;
+            // eww, there has to be a better way to handle this
+            let title = path.file_stem().map_or("thread.run", |s| s.to_str().unwrap());
+            let generated = pretend_template(title, &rendered);
+            return Ok(Content(ContentType::HTML, generated));
         }
     }
 
     let mut contents = String::new();
-    File::open(Path::new(SITE_ROOT).join(path))?.read_to_string(&mut contents)?;
-    Ok(contents)
+    File::open(&full_path)?.read_to_string(&mut contents)?;
+
+    let content_type = match path.extension().map_or(None, |s| s.to_str()) {
+        // TODO handle images
+        Some("css") => ContentType::CSS,
+        // TODO load unknown files to a binary container so the default
+        // can be non-text
+        _ => ContentType::Plain,
+    };
+
+    Ok(Content(content_type, contents))
 }
 
 fn rocket() -> rocket::Rocket {
@@ -39,3 +57,20 @@ fn get_md_as_html(path: &Path) -> io::Result<String> {
 
     Ok(markdown_to_html(&contents, &ComrakOptions::default()))
 }
+
+
+fn pretend_template(title: &str, content: &str) -> String {
+    // I would rather this be in a constant but format! requires a string literal?
+    // TODO fix this
+    format!(r#"
+<html>
+<head>
+    <title>{title}</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    {body}
+</body>
+"#, title = title, body = content)
+}
+
