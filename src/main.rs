4dcbf1d000e3463640ff7903f41bf4c9483e34d2
Alex Addy <alex.addy@gmail.com>
2018-01-01 11:30:29 -0500

    Simple hardcoded test for file returning

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,21 @@
 
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use std::path::{Path, PathBuf};
+
+use rocket::response::NamedFile;
+
+#[get("/<path..>")]
+fn pages(path: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new(SITE_ROOT).join(path)).ok()
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![pages])
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
 }
+
+const SITE_ROOT: &'static str = "../thread.run/";
