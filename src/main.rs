088fbb29df60408072a85658719dfb303da127c5
Alex Addy <alex.addy@gmail.com>
2018-01-01 14:55:29 -0500

    Markdown is now rendered before being sent toe the client

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,24 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate comrak;
 
+use std::fs::File;
+use std::io;
+use std::io::Read;
 use std::path::{Path, PathBuf};
 
-use rocket::response::NamedFile;
-
 #[get("/<path..>")]
-fn pages(path: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new(SITE_ROOT).join(path)).ok()
+fn pages(path: PathBuf) -> io::Result<String> {
+    if let Some(ext) = path.extension() {
+        if ext == "html" {
+            return get_md_as_html(&Path::new(SITE_ROOT).join(path.with_extension("md")))
+        }
+    }
+
+    let mut contents = String::new();
+    File::open(Path::new(SITE_ROOT).join(path))?.read_to_string(&mut contents)?;
+    Ok(contents)
 }
 
 fn rocket() -> rocket::Rocket {
@@ -21,3 +31,11 @@ fn main() {
 }
 
 const SITE_ROOT: &'static str = "../thread.run/";
+
+use comrak::{markdown_to_html, ComrakOptions};
+fn get_md_as_html(path: &Path) -> io::Result<String> {
+    let mut contents = String::new();
+    File::open(path)?.read_to_string(&mut contents)?;
+
+    Ok(markdown_to_html(&contents, &ComrakOptions::default()))
+}
